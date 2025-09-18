185858e447728768c35d06d5438cb2d1fe3b41dd
Shivanand Pujari <154795271+shivanand143@users.noreply.github.com>
2025-09-19 01:42:32 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,14 +2,15 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-  const token = req.cookies.token; // or Authorization header
+  const token = req.cookies.token; 
   if (!token) return res.status(401).json({ msg: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password"); // remove password
+    req.user = await User.findById(decoded.id).select("-password");
     next();
   } catch (err) {
     res.status(401).json({ msg: "Invalid token" });
   }
 };
+
