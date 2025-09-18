6fc3def163cfa54596e573b3a14d60cc522e0962
Shivanand Pujari <154795271+shivanand143@users.noreply.github.com>
2025-09-19 01:38:47 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://lead-management-system-s6lh.onrender.com/",
+  baseURL: "https://lead-management-system-s6lh.onrender.com/api/",
   withCredentials: true,
 });
 
 export default API;
+
 
 
